b7796bc1253786b9e8bb374607d5d7426bb6bd28
Fabricio Pirini <fabricio.pirini@gmail.com>
2022-04-11 21:17:19 +0200

    Add AuthId to Customer and add create product

--- purchases/src/http/graphql/resolvers/purchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,11 +1,22 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+
+import { CustomersService } from '../../../services/customers.service';
 import { ProductsService } from '../../../services/products.service';
-
 import { PurchasesService } from '../../../services/purchases.service';
-import { AuthorizationGuard } from '../../auth/authorization.guard';
-import { Product } from '../models/product';
 
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { AuthUser, CurrentUser } from '../../auth/current-user';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
+
+import { Product } from '../models/product';
 import { Purchase } from '../models/purchase';
 
 @Resolver(() => Purchase)
@@ -13,6 +24,7 @@ export class PurchaseResolver {
   constructor(
     private purchasesService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomersService,
   ) {}
 
   @Query(() => [Purchase])
@@ -24,5 +36,27 @@ export class PurchaseResolver {
   @ResolveField(() => Product)
   product(@Parent() purchase: Purchase) {
     return this.productsService.getProductById(purchase.productId);
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(AuthorizationGuard)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      productId: data.productId,
+      customerId: customer.id,
+    });
   }
 }
