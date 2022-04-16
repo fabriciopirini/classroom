6d58b41b8b7bb7c24bf38f2ec8bbc3547d18c943
Fabricio Pirini <fabricio.pirini@gmail.com>
2022-04-16 19:23:07 +0200

    Add Apollo Codegen to the Web

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,12 +1,17 @@
+import { gql, useQuery } from "@apollo/client";
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Image from "next/image";
 import Link from "next/link";
+import { useGetProductsQuery } from "../../graphql/generated/graphql";
 
-export default function Home() {
-  const { user, error, isLoading } = useUser();
+import { withApollo } from "../../lib/withApollo";
+
+function Home() {
+  const { user, error: fetchUserError, isLoading } = useUser();
+  const { data, loading, error: queryError } = useGetProductsQuery();
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+  if (fetchUserError) return <div>{fetchUserError.message}</div>;
 
   return (
     <div>
@@ -21,8 +26,18 @@ export default function Home() {
       ) : (
         <Link href="/api/auth/login">Login</Link>
       )}
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async ({ req, res }) => {
+    return {
+      props: {},
+    };
+  },
+});
+
+export default withApollo(Home);
